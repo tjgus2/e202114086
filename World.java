6b00a3ecf6d714ed3d97c4fc2c5922c3ecc2c914
Park, Seohyun <tjgus6279@naver.com>
2024-09-25 09:35:58 +0900

    fifth

--- World.java
+++ World.java
@@ -4,4 +4,8 @@ public class World {
       System.out.println("202114086"); 
     } 
    
+    public static void run2() { 
+      System.out.println("hello world"); 
+    } 
+   
   } 
--- src/e202114086_/Main.java
+++ src/e202114086_/Main.java
@@ -1,7 +1,11 @@
 package e202114086_;
 
 public class Main { 
-public static void main(String[] args) { 
-System.out.println("박서현"); 
-} 
-} 
+ 
+    public static void main(String[] args) { 
+      System.out.println("박서현"); 
+      World.run(); 
+      World.run2(); 
+    } 
+   
+  } 
